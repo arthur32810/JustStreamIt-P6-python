f9ec17aa2324b01050b599dace6b1841541ec702
arthur <arthur32810@gmail.com>
2025-03-18 17:31:22 +0100

    creation du meilleur film en js

--- assets/js/files/films.js
+++ assets/js/files/films.js
@@ -1,4 +1,32 @@
 //Créer la structure html d'une card film
+export function createCardBestFilm(film) {
+    const article = document.createElement('article')
+    article.id = "film-top"
+
+    const imgFilm = document.createElement("img")
+    imgFilm.alt = "Image du film " + film.image_url
+    imgFilm.src = film.image_url
+
+    article.appendChild(imgFilm)
+
+    const div = document.createElement("div")
+    const titleFilm = document.createElement("h3")
+    titleFilm.textContent = film.title
+
+    const descriptionFilm = document.createElement("p")
+    descriptionFilm.textContent = film.description
+
+    const button = document.createElement("button")
+    button.textContent = "Détails"
+
+    div.appendChild(titleFilm)
+    div.appendChild(descriptionFilm)
+    div.appendChild(button)
+
+    article.appendChild(div)
+    return article
+}
+
 export function createCardFilm(film) {
     const article = document.createElement('article')
     article.classList.add('card-film')
--- assets/js/files/main.js
+++ assets/js/files/main.js
@@ -1,10 +1,16 @@
-import { createCardFilm } from "./films.js";
+import { createCardBestFilm, createCardFilm } from "./films.js";
+
+const topfilmContainer = document.getElementById("film-top")
+const topFilm = { "title": "The Big Lebowski", "image_url": "./assets/images/film-top.png", "description": 'Jeff "The Dude" Lebowski, mistaken for a millionaire of the same name, seeks restitution for his ruined rug and enlists his bowling buddies to help get it.' }
+const cardTopFilm = createCardBestFilm(topFilm)
+
+topfilmContainer.parentNode.replaceChild(cardTopFilm, topfilmContainer)
 
 //récupération container film mieux noté
-const bestFilm = document.getElementById("best-films")
+const bestFilmContainer = document.getElementById("best-films")
 const titanic = { "title": "Titanic", "image_url": "./assets/images/titanic-1.png" }
 const cardTitanic = createCardFilm(titanic)
 
 for (let i = 0; i < 6; i++) {
-    bestFilm.appendChild(createCardFilm(titanic))
+    bestFilmContainer.appendChild(createCardFilm(titanic))
 }
